48f04ee177bb40d74acc6fa96acf319b936feb0a
alexisimo <alexis0023@hotmail.com>
2025-04-10 15:03:54 +0000

    Get and define real cases tests

--- get_data.sh
+++ get_data.sh
@@ -95,12 +95,7 @@ for f in data/vocabularies/*.ttl; do (cat "${f}"; echo) >> data/vocabularies/voc
 cat data/vocabularies/vocabularies.ttl data/raw/2025-01-05-rinf-xml-combined.nt > data/ERA.ttl
 cat data/vocabularies/vocabularies.ttl data/raw/ES.nt > data/ES.ttl
 cat data/vocabularies/vocabularies.ttl data/raw/FR.nt > data/FR.ttl
-cat data/vocabularies/vocabularies.ttl data/raw/LV.nt > data/LV.ttl
-
-# Free disk space
-echo "Cleaning directories..."
-rm -r data/raw
-rm -r data/vocabularies
+# cat data/vocabularies/vocabularies.ttl data/raw/LV.nt > data/LV.ttl
 
 # Get shapes
 mkdir shapes
@@ -114,5 +109,84 @@ echo "Subsetting SHACL shapes into core and train detection systems..."
 sed '/era-sh:.*sparql/,/.*""".*.$/d' shapes/era_shapes.ttl > shapes/core_shapes.ttl
 # Subset shapes for train detection systems
 sed -n 4381,4796p shapes/era_shapes.ttl > shapes/tds_shapes.ttl
+
+# Create test subsets
+echo "Getting maxCount-era-002 test resources..."
+echo "# maxCount-era-002 valid resource" > tests/core/property/maxCount-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/trainDetectionSystems/ESL400400010_ES51101_II_ES51111_track%20circuit>" data/ERA.ttl >> tests/core/property/maxCount-era-002-data.ttl
+echo "# maxCount-era-002 invalid resource" >> tests/core/property/maxCount-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/trainDetectionSystems/ESL512007300_ES42020_U_ES43023_wheel%20detector>" data/ERA.ttl >> tests/core/property/maxCount-era-002-data.ttl
+
+echo "Getting class-era-001 test resources..."
+echo "# class-era-001 valid resource" > tests/core/property/class-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/operationalPoints/0b940a5320dee6993ae2e90f88538465f0f3c0bc>" data/ERA.ttl >> tests/core/property/class-era-001-data.ttl
+grep "<http://data.europa.eu/949/documents/b4e2b5340d9c2086affeabbf1f6f427f0112ccdd>" data/ERA.ttl >> tests/core/property/class-era-001-data.ttl
+echo "# class-era-001 invalid resource" >> tests/core/property/class-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/operationalPoints/a7a2ce92523c7c250b0bb25a6e7a2a31b82acc8c>" data/ERA.ttl >> tests/core/property/class-era-001-data.ttl
+grep "<http://data.europa.eu/949/documents/0b03113a9f7af9051ee710609f82ee65a1421bbe>" data/ERA.ttl >> tests/core/property/class-era-001-data.ttl
+
+echo "Getting path-sequence-era-001 test resources..."
+echo "# path-sequence-era-001 valid resource" > tests/core/path/path-sequence-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/21ffaaa0f33d609cbd6f672df6f3f34927b90047>" data/ERA.ttl >> tests/core/path/path-sequence-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/Vilnius-Kena-BY_LTKena_II_EU00250_distancetypeMAXlength270switchoffbreakerYlowerpantographNchangesupplysystemNkm034.780>" data/ERA.ttl >> tests/core/path/path-sequence-era-001-data.ttl
+echo "# path-sequence-era-001 invalid resource" >> tests/core/path/path-sequence-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/36bf1461f295d432dd780fdc507133f908b7a1d4>" data/ERA.ttl >> tests/core/path/path-sequence-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/ESL984610276_ESA0849_II%20MIXTA_ES08207_length402switchoffbreakerYlowerpantographN>" data/ERA.ttl >> tests/core/path/path-sequence-era-001-data.ttl
+
+echo "Getting datatype-era-002 test resources..."
+echo "# datatype-era-002 valid resource" > tests/core/property/datatype-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/21ffaaa0f33d609cbd6f672df6f3f34927b90047>" data/ERA.ttl >> tests/core/property/datatype-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/Vilnius-Kena-BY_LTKena_II_EU00250_distancetypeMAXlength270switchoffbreakerYlowerpantographNchangesupplysystemNkm034.780>" data/ERA.ttl >> tests/core/property/datatype-era-002-data.ttl
+echo "# datatype-era-002 invalid resource" >> tests/core/property/datatype-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/36bf1461f295d432dd780fdc507133f908b7a1d4>" data/ERA.ttl >> tests/core/property/datatype-era-002-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/ESL984610276_ESA0849_II%20MIXTA_ES08207_length402switchoffbreakerYlowerpantographN>" data/ERA.ttl >> tests/core/property/datatype-era-002-data.ttl
+
+echo "Getting or-era-001 test resources..."
+echo "# or-era-001 valid resource" > tests/core/property/or-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/21ffaaa0f33d609cbd6f672df6f3f34927b90047>" data/ERA.ttl >> tests/core/property/or-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/Vilnius-Kena-BY_LTKena_II_EU00250_distancetypeMAXlength270switchoffbreakerYlowerpantographNchangesupplysystemNkm034.780>" data/ERA.ttl >> tests/core/property/or-era-001-data.ttl
+echo "# or-era-001 invalid resource" >> tests/core/property/or-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/36bf1461f295d432dd780fdc507133f908b7a1d4>" data/ERA.ttl >> tests/core/property/or-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/ESL984610276_ESA0849_II%20MIXTA_ES08207_length402switchoffbreakerYlowerpantographN>" data/ERA.ttl >> tests/core/property/or-era-001-data.ttl
+
+echo "Getting pattern-era-001 test resources..."
+echo "# pattern-era-001 valid resource" > tests/core/property/pattern-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/sidings/03f5ac96ec17387fef12b06e302b74893b4ec932>" data/ERA.ttl >> tests/core/property/pattern-era-001-data.ttl
+echo "# pattern-era-001 invalid resource" >> tests/core/property/pattern-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/sidings/41f97688067c71186b284ba9c7b85c1e259167df>" data/ERA.ttl >> tests/core/property/pattern-era-001-data.ttl
+
+echo "Getting maxExclusive-era-001 test resources..."
+echo "# maxExclusive-era-001 valid resource" > tests/core/property/maxExclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B5.0966/52.0988>" data/ERA.ttl >> tests/core/property/maxExclusive-era-001-data.ttl
+echo "# maxExclusive-era-001 invalid resource" >> tests/core/property/maxExclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B49.2266/16.6055>" data/ERA.ttl >> tests/core/property/maxExclusive-era-001-data.ttl
+
+echo "Getting minInclusive-era-001 test resources..."
+echo "# minInclusive-era-001 invalid resource" > tests/core/property/minInclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B49.2266/16.6055>" data/ERA.ttl >> tests/core/property/minInclusive-era-001-data.ttl
+echo "# minInclusive-era-001 valid resource" >> tests/core/property/minInclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B5.0966/52.0988>" data/ERA.ttl >> tests/core/property/minInclusive-era-001-data.ttl
+
+echo "Getting sparql-era-001 test resources..."
+echo "# ontology and train protection legacy systems skos concepts" > tests/sparql/property/sparql-era-001-data.ttl
+cat data/vocabularies/ontology.ttl data/vocabularies/era-skos-TrainProtectionLegacySystems.ttl >> tests/sparql/property/sparql-era-001-data.ttl
+echo "# sparql-era-001 valid resource" >> tests/sparql/property/sparql-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/07946c41d18b51d16a9fa7013dc13d30e3374c39>" data/ERA.ttl >> tests/sparql/property/sparql-era-001-data.ttl
+echo "# sparql-era-001 invalid resource" >> tests/sparql/property/sparql-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/tracks/36bf1461f295d432dd780fdc507133f908b7a1d4>" data/ERA.ttl >> tests/sparql/property/sparql-era-001-data.ttl
+grep "<http://data.europa.eu/949/functionalInfrastructure/phaseInfo/ESL984610276_ESA0849_II%20MIXTA_ES08207_length402switchoffbreakerYlowerpantographN>" data/ERA.ttl >> tests/sparql/property/sparql-era-001-data.ttl
+
+# Modified shapes to test other features
+echo "Getting minInclusive-era-001 test resources..."
+echo "# minInclusive-era-001 invalid resource" > tests/core/node/minInclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B49.2266/16.6055>" data/ERA.ttl >> tests/core/node/minInclusive-era-001-data.ttl
+echo "# minInclusive-era-001 valid resource" >> tests/core/node/minInclusive-era-001-data.ttl
+grep "<http://data.europa.eu/949/locations/%2B5.0966/52.0988>" data/ERA.ttl >> tests/core/node/minInclusive-era-001-data.ttl
+
+
+# Free disk space
+echo "Cleaning directories..."
+rm -r data/raw
+rm -r data/vocabularies
 
 echo "Done."
--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for engine in maplib jena topbraid rdf4j rdfunit dotnet_rdf pyshacl corese trav_shacl; do # $(ls -d engines/*/ | cut -f2 -d'/'); do 
-    echo $engine
-    mkdir ./results/$engine
-    mkdir ./results/$engine/reports
-    echo "Test" $engine
-    docker run -d --rm --name $engine-container-test -v $(pwd)/data/test:/data -v $(pwd)/shapes/test:/shapes -v $(pwd)/results/$engine/reports:/reports  $engine-validation-experiment:latest ../data/test.ttl ../shapes/test_shapes.ttl ../reports/test_report.ttl 
+for engine in jena; do # maplib jena topbraid rdf4j rdfunit dotnet_rdf pyshacl corese trav_shacl; do # $(ls -d engines/*/ | cut -f2 -d'/'); do 
+    # mkdir -p ./results/$engine/reports/sparql/property
+    mkdir -p ./results/$engine/reports/core/property
+
+    echo "Testing" $engine "features"
+    for test in ./tests/*/node/minInclusive-era-001-data.ttl; do #./tests/*/*/*-data.ttl; do
+        report_dir=$(dirname $test| sed "s|./tests|results/$engine/reports|")
+        test_data_path=$(echo $test| sed "s|./tests/||")
+        test_shapes_path=$(echo $test_data_path| sed "s|-data||")
+        filename=$(basename $test_shapes_path .ttl)
+        mkdir -p $report_dir
+        echo "->" $filename
+        docker run --rm --name $engine-container-test-$filename -v $(pwd)/tests:/data -v $(pwd)/$report_dir:/reports  $engine-validation-experiment:latest ../data/$test_data_path ../data/$test_shapes_path ../reports/$filename-report.ttl
+    done
 done
