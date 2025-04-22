b37234cb3b2e5fa38d33125f4ca0f154fbe959a8
alexisimo <alexis0023@hotmail.com>
2025-04-22 09:38:50 +0000

    Updated maplib script with base iri and ttl serialization

--- engines/maplib/validate.py
+++ engines/maplib/validate.py
@@ -20,18 +20,18 @@ def maplib_validate(DATA, SHAPES, REPORT):
     m = Mapping()
     # Start measuring the loading time
     load_tic = time.time()
-    m.read_triples(DATA)
+    m.read_triples(DATA, base_iri="http://example.net/")
     load_tictoc = time.time() - load_tic
     print("Load time: ", load_tictoc)
 
     # Parse shacl shapes to rdflib graph 
     shape_graph = "urn:eu:shacl"
-    m.read_triples(SHAPES, graph=shape_graph)
+    m.read_triples(SHAPES, graph=shape_graph, base_iri="http://example.net/")
 
     # SHACL validation block
     # Measuring the validation time
     tic = time.time()
-    report = m.validate(shape_graph=shape_graph)
+    report = m.validate(shape_graph=shape_graph, include_shape_graph=False)
     tictoc = time.time() - tic
     print( "Validation time: ", tictoc)
 
@@ -40,8 +40,9 @@ def maplib_validate(DATA, SHAPES, REPORT):
 
     vres_graph = rdflib.Graph()
     with open(os.path.splitext(REPORT)[0]+".nt", "r", encoding="utf-8") as file:
-        content = re.sub(r'\\"', '"', file.read()) # fixing an error in maplib nt writer
-        vres_graph.parse(data=content, format="nt")
+    #     content = re.sub(r'\\"', '"', file.read()) # fixing an error in maplib nt writer
+    #     vres_graph.parse(data=content, format="nt")
+        vres_graph.parse(data=file.read(),format="nt")    
     vres_graph.serialize(format="turtle", destination=REPORT)
 
 maplib_validate(args.data, args.shapes, args.report)
